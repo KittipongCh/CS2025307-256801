234d3ed0bbe8371ac2e6f194bffb533923e2ec11
Kittipong Chinsook <36752630+KittipongCh@users.noreply.github.com>
2025-09-15 18:03:16 +0700

    Lab 03
    openGL

---
+++ lab03/main.cpp
@@ -0,0 +1,99 @@
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+#include <string>
+#include <fstream>
+
+#define numVAOs 1
+
+using namespace std;
+
+GLuint renderingProgram;
+GLuint vao[numVAOs];
+
+string readFile(const char *filePath){
+    string content;
+    ifstream fileStream(filePath, ios::in);
+    string line = "";
+
+    while(!fileStream.eof()){
+        getline(fileStream, line);
+        content.append(line + "\n");
+    }
+
+    fileStream.close();
+    return content;
+}
+
+GLuint createShaderProgram(){
+    GLuint vShader = glCreateShader(GL_VERTEX_SHADER);
+    GLuint fShader = glCreateShader(GL_FRAGMENT_SHADER);
+
+    string vertShaderStr = readFile("vertexShader.glsl");
+    string fragShaderStr = readFile("fragmentShader.glsl");
+    const char *vertShaderSrc = vertShaderStr.c_str();
+    const char *fragShaderSrc = fragShaderStr.c_str();
+
+    glShaderSource(vShader, 1, &vertShaderSrc,NULL);
+    glShaderSource(fShader, 1, &fragShaderSrc, NULL);
+    glCompileShader(vShader);
+    glCompileShader(fShader);
+
+    GLuint vfProgram = glCreateProgram();
+    glAttachShader(vfProgram, vShader);
+    glAttachShader(vfProgram, fShader);
+    glLinkProgram(vfProgram);
+
+    return vfProgram;
+}
+
+void init(GLFWwindow* window) {
+    renderingProgram = createShaderProgram();
+    glGenVertexArrays(numVAOs, vao);
+    glBindVertexArray(vao[0]);
+}
+
+void display(GLFWwindow* window, double currentTime) {
+    glClearColor(0.0, 0.0, 0.0, 1.0);
+    glClear(GL_COLOR_BUFFER_BIT);
+    glUseProgram(renderingProgram);
+    //glPointSize(10.0);
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+}
+
+int main(void)
+{
+    /* Initialize the library */
+    if (!glfwInit()){
+        exit(EXIT_FAILURE);
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+
+    /* Create a windowed mode window and its OpenGL context */
+    GLFWwindow* window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+    if (glewInit() != GLEW_OK){
+        exit(EXIT_FAILURE);
+    }
+    glfwSwapInterval(1);
+
+    init(window);
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window)){
+        display(window, glfwGetTime());
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    exit(EXIT_SUCCESS);
+}
