067e82ec209613b5cb259fc56b9c396dd6374eb0
Kittipong Chinsook <36752630+KittipongCh@users.noreply.github.com>
2025-09-18 20:56:19 +0700

    Lab Lecture 07
    openGL: Orthographics

---
+++ lec07-Triangle-LookAt-Ortho/vertexShader.glsl
@@ -0,0 +1,15 @@
+#version 330
+layout (location = 0) in vec3 aPos;
+layout (location = 1) in vec3 aColor;
+
+//uniform mat4 uModelMatrix;
+//uniform mat4 uViewMatrix;
+uniform mat4 uProjMatrix;
+
+out vec3 outColor;
+
+void main(void)
+{
+    gl_Position = uProjMatrix * vec4(aPos, 1.0);
+    outColor = aColor;
+}
