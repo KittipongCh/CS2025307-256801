9eb1cc498c2f327bd1ebac5f983e9868a9a48807
Kittipong Chinsook <36752630+KittipongCh@users.noreply.github.com>
2025-09-18 20:50:48 +0700

    Lab Lecture 07
    OpenGL: 3D - Cube version 10
    Reference: Learn OpenGL (URL: https://learnopengl.com)

---
+++ lec07-Cube-v10-Texture/vertexShader.glsl
@@ -0,0 +1,18 @@
+#version 330
+layout (location = 0) in vec3 aPos;
+layout (location = 1) in vec3 aColor;
+layout (location = 2) in vec2 aTexCoord;
+
+//uniform mat4 uModelMatrix;
+uniform mat4 uViewMatrix;
+uniform mat4 uProjMatrix;
+
+out vec3 outColor;
+out vec2 TextureCoord;
+
+void main(void)
+{
+    gl_Position = uProjMatrix * uViewMatrix * vec4(aPos, 1.0);
+    outColor = aColor;
+    TextureCoord = aTexCoord;
+}
