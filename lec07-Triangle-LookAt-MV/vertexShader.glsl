c69aaa7efd3faf116f8b2eb653a840285e4fc6de
Kittipong Chinsook <36752630+KittipongCh@users.noreply.github.com>
2025-09-18 20:54:35 +0700

    Lab Lecture 07
    openGL: LookAt (Model View)

---
+++ lec07-Triangle-LookAt-MV/vertexShader.glsl
@@ -0,0 +1,14 @@
+#version 330
+layout (location = 0) in vec3 aPos;
+layout (location = 1) in vec3 aColor;
+
+uniform mat4 uModelMatrix;
+uniform mat4 uViewMatrix;
+
+out vec3 outColor;
+
+void main(void)
+{
+    gl_Position = uViewMatrix * uModelMatrix * vec4(aPos, 1.0);
+    outColor = aColor;
+}
